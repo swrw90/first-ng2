ae1ef0b7da36cacdc0358e31ba843d95b18bb1f6
Seth Watson <seth.watson1@gmail.com>
2017-09-20 20:44:46 -0700

    ngFor practice

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,12 +3,17 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-server',
     templateUrl: '/server.component.html',
+    styles: [`
+    .online {
+        color: white;
+        }
+    `]
 })
 export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = 'offline';
     constructor() {
-        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';    
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
     }
 
     getServerStatus() {
